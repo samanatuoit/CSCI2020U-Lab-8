c8a0919ed4f9f9470ad2cb56d35c468f50fa504b
samanatuoit <saman.yaghini@uoit.net>
2016-03-17 10:02:04 -0400

    Optional section: Added UI section

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -55,14 +55,39 @@ public class Main extends Application {
         table.getColumns().add(letterGradeColumn);
 
 
+        /* GridPane fng a new student */
+        Label sidLbl = new Label("SID: ");
+        TextField sidFld = new TextField();
+        Label midtermLbl = new Label("Midterm: ");
+        TextField midtermFld = new TextField();
+        Label assignLbl = new Label("Assignments: ");
+        TextField assignFld = new TextField();
+        Label finalExamLbl = new Label("Final Exam: ");
+        TextField finalExamFld = new TextField();
+        Button addBtn = new Button("Add");
+
+        GridPane addPane = new GridPane();
+        addPane.setHgap(20);
+        addPane.setVgap(20);
+
+        addPane.add(sidLbl, 0, 0);
+        addPane.add(sidFld, 1, 0);
+        addPane.add(midtermLbl, 0, 1);
+        addPane.add(midtermFld, 1, 1);
+        addPane.add(assignLbl, 2, 0);
+        addPane.add(assignFld, 3, 0);
+        addPane.add(finalExamLbl, 2, 1);
+        addPane.add(finalExamFld, 3, 1);
+        addPane.add(addBtn, 1, 3);
 
 
 
 
 
         layout = new BorderPane();
+        layout.setBottom(addPane);
         layout.setCenter(table);
-        primaryStage.setScene(new Scene(layout, 600, 600));
+        primaryStage.setScene(new Scene(layout, 600, 400));
         primaryStage.show();
     }
 
