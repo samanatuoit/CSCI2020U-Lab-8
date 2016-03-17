c91852a75020d7493fccfe31ce3956d8c9bbee73
samanatuoit <saman.yaghini@uoit.net>
2016-03-17 08:28:00 -0400

    DataSource class added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private BorderPane layout;
+    private TableView<StudentRecord> table;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Lab 05");
+
+        table = new TableView<>();
+        //table.setItems();
+
+
+
+
+        layout = new BorderPane();
+        primaryStage.setScene(new Scene(layout, 300, 275));
         primaryStage.show();
     }
 
